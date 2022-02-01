d5aa5e48b8b1f3055ea5bf9719975b24dc25bf88
Taite Jernigan <taite5100@gmail.com>
2022-01-31 19:07:12 -1000

    Successfully got data from the API, transformed data, and posted transformed data back to the API with a success message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getDatasetId, getVehicleIds, getVehicleData, getDealerData, postAnswer } from '../../server/API.js';
 
 function App(props) {
+
+  useEffect(() => {
+    getDatasetId((datasetId) => {
+      console.log(datasetId)
+      getVehicleIds(datasetId, (vehicleIds) => {
+        console.log(vehicleIds)
+        getVehicleData(datasetId, vehicleIds, (vehicleData) => {
+          console.log(vehicleData)
+          let dealerArr = []
+          vehicleData.map(({dealerId}) => {
+            if(dealerArr.indexOf(dealerId) === -1) {
+              dealerArr.push(dealerId);
+            }
+          })
+          getDealerData(datasetId, dealerArr, (dealerData) => {
+            console.log(dealerData)
+            let answer = {
+              dealers: dealerData
+            }
+            answer.dealers.forEach((dealership) => {
+              dealership.vehicles = [];
+              vehicleData.map((data) => {
+                if (data.dealerId === dealership.dealerId) {
+                  dealership.vehicles.push({
+                    vehicleId: data.vehicleId,
+                    year: data.year,
+                    make: data.make,
+                    model: data.model
+                  })
+                }
+              })
+            })
+            postAnswer(datasetId, answer, (response) => {
+              console.log(response)
+            })
+          })
+        })
+      })
+    })
+  }, [])
+
   return (
     <h1>Hello world</h1>
   )
--- server/API.js
+++ server/API.js
@@ -1,16 +1,36 @@
 const axios = require('axios');
 
 module.exports = {
-  postShirt: (data, cb) => {
-    axios.post('http://localhost:5000/api/shirts', data)
-    .then(data => cb(data.data))
+  getDatasetId: (cb) => {
+    axios.get('http://api.coxauto-interview.com/api/datasetId')
+    .then(({ data }) => cb(data.datasetId))
   },
-  getAllShirts: (cb) => {
-    axios.get('http://localhost:5000/api/shirts')
-    .then(data => cb(data.data))
+  getVehicleIds: (datasetId, cb) => {
+    axios.get(`http://api.coxauto-interview.com/api/${datasetId}/vehicles`)
+    .then(({ data }) => cb(data.vehicleIds))
   },
-  likeShirt: (id, cb) => {
-    axios.put('http://localhost:5000/api/like', { id })
-    .then(data => cb(data.data))
+  getVehicleData: (datasetId, vehicleIds, cb) => {
+    axios.all(vehicleIds.map((carId) => axios.get(`http://api.coxauto-interview.com/api/${datasetId}/vehicles/${carId}`)))
+    .then(carData => {
+      let carDataArr = [];
+      carData.map(({ data }) => {
+        carDataArr.push(data)
+      })
+      cb(carDataArr)
+    })
+  },
+  getDealerData: (datasetId, dealerIds, cb) => {
+    axios.all(dealerIds.map((dealerId) => axios.get(`http://api.coxauto-interview.com/api/${datasetId}/dealers/${dealerId}`)))
+    .then(dealerData => {
+      let dealerDataArr = [];
+      dealerData.map(({ data }) => {
+        dealerDataArr.push(data);
+      })
+      cb(dealerDataArr)
+    })
+  },
+  postAnswer: (datasetId, answer, cb) => {
+    axios.post(`http://api.coxauto-interview.com/api/${datasetId}/answer`, answer)
+    .then(({data}) => cb(data))
   }
 }
